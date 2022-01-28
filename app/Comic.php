8e703513486c5b44af2ada8bed15ee8c402bf618
francesco <francescosanti91@gmail.com>
2022-01-28 16:07:30 +0100

    add comics seeder

---
+++ app/Comic.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comic extends Model
+{
+    protected $fillable = ['title', 'author', 'release_date', 'pages'];
+}
---
+++ database/factories/ComicFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Comic;
+use Faker\Generator as Faker;
+
+$factory->define(Comic::class, function (Faker $faker) {
+    return [
+        'title' => $faker -> sentence(2), 
+        'author' => $faker -> name(), 
+        'release_date' => $faker -> date(), 
+        'pages' => $faker -> numberBetween(80, 1500)
+    ];
+});
